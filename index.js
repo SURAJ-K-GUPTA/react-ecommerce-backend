01487a73c132bab5f794608b813b55db2b0bb8f6
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2023-12-10 22:00:41 +0530

    Backend Part 1

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const { default: mongoose } = require('mongoose');
-
+const { createProduct } = require('./controller/Product');
+const productsRouters = require('./routes/Products')
+const categoriesRouter = require('./routes/Categories')
+const brandsRouter = require('./routes/Brands')
 const server = express();
+const cors = require('cors')
+//middelewares
+server.use(cors({
+    exposedHeaders:['X-Total-Count']
+}))
+server.use(express.json()); // to parse req.body
+server.use('/products', productsRouters.router)
+server.use('/categories', categoriesRouter.router)
+server.use('/brands', brandsRouter.router)
 
+main().then(console.log("DB Connected")).catch(err=>console.log(err))
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/react-ecommerce')
+}
 server.get('/',(req,res)=>{
     res.json({status:'success'})
 })
 
-server.listen(9000,()=>{
-    console.log('server started')
+
+server.listen(8080,()=>{
+    console.log(`Server Started at PORT 8080`)
 })
---
+++ routes/Products.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  createProduct,
+  fetchAllProducts,
+  fetchProductById,
+  updateProductById,
+} = require("../controller/Product");
+
+const router = express.Router();
+
+// /products is already added in base path
+router
+  .post("/", createProduct)
+  .get("/", fetchAllProducts)
+  .get("/:id", fetchProductById)
+  .patch("/:id", updateProductById);
+
+exports.router = router;
