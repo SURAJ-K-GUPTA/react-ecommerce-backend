2a8dc17619ecbc829500e27a4af03daca4a6b95d
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2023-12-17 00:48:58 +0530

    commiting code with some csp error in stripe

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ server.use(
     exposedHeaders: ["X-Total-Count"],
   })
 );
+server.use(express.raw({type: 'application/json'}))
 server.use(express.json()); // to parse req.body
 server.use("/products", isAuth(), productsRouters.router); // we can also use JWT token for client-only auth
 server.use("/categories", isAuth(), categoriesRouter.router);
@@ -129,6 +130,99 @@ passport.deserializeUser(function (user, cb) {
   });
 });
 
+// Payments
+
+// This is your test secret API key.
+const stripe = require("stripe")('sk_test_51ONgfFSDhB1iNrv7LFhYVPSoAkGEui6Tj5tnIYWzKnNbV8CPgiK0p1fRwqXuTc5sYMWkz5uR50gEZ7c0jkaNk9kD008vkfHfyF');
+
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400; // this is 14.00
+};
+
+server.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "inr",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+
+// Webhook
+
+// TODO: we will capture actual order after deploying out server live on public URL
+// This is your test secret API key.
+// const stripe = require('stripe')('sk_test_51ONgfFSDhB1iNrv7LFhYVPSoAkGEui6Tj5tnIYWzKnNbV8CPgiK0p1fRwqXuTc5sYMWkz5uR50gEZ7c0jkaNk9kD008vkfHfyF');
+// Replace this endpoint secret with your endpoint's unique secret
+// If you are testing with the CLI, find the secret by running 'stripe listen'
+// If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+// at https://dashboard.stripe.com/webhooks
+const endpointSecret = 'whsec_7285fc3dee9a062c58b748163bb4ed6b0a0a7bf06c702e30c09ffed4228ba83e';
+// const express = require('express');
+// const app = express();
+
+server.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  let event = request.body;
+  // Only verify the event if you have an endpoint secret defined.
+  // Otherwise use the basic event deserialized with JSON.parse
+  if (endpointSecret) {
+    // Get the signature sent by Stripe
+    const signature = request.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
+    }
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log({paymentIntent})
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+// app.listen(4242, () => console.log('Running on port 4242'));
+
+
+
+
+// main
 main()
   .then(console.log("DB Connected"))
   .catch((err) => console.log(err));
